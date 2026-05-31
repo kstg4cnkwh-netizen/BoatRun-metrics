1e21d9a216232a3e2b0b7b93c6811fff1bf5df2e
kstg4cnkwh-netizen <kstg4cnkwh@privaterelay.appleid.com>
2026-05-31 13:14:03 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const C = 'scull-v20';
+const URLS = [
+  './',
+  './Metrics_260531_1.html'   // add actual filename
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
